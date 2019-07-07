b201c2debef60553fe46b2a6acc7cdb4ccc95949
TennyZhuang <zty0826@gmail.com>
2019-07-08 01:36:51 +0800

    finish merge sort

--- tidb/mergesort/mergesort.go
+++ tidb/mergesort/mergesort.go
@@ -3,4 +3,31 @@ package main
 // MergeSort performs the merge sort algorithm.
 // Please supplement this function to accomplish the home work.
 func MergeSort(src []int64) {
+	const maxInt64 = 9223372036854775807
+
+	n := len(src)
+	if n == 1 {
+		return
+	}
+	m := n / 2
+	MergeSort(src[:m])
+	arr1 := make([]int64, m+1)
+	copy(arr1, src[:m])
+	arr1[m] = maxInt64
+
+	MergeSort(src[m:])
+	arr2 := make([]int64, n-m+1)
+	copy(arr2, src[m:])
+	arr2[n-m] = maxInt64
+
+	i, j := 0, 0
+	for k := 0; k < n; k++ {
+		if arr1[i] <= arr2[j] {
+			src[k] = arr1[i]
+			i++
+		} else {
+			src[k] = arr2[j]
+			j++
+		}
+	}
 }
